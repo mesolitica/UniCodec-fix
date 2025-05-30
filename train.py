b3ce90432d77a90fe616485d2ac26eab18b5a7ec
Yidi Jiang <yidi_jiang@u.nus.edu>
2025-05-30 14:40:18 +0800

    update

--- train.py
+++ train.py
@@ -4,9 +4,7 @@ from pytorch_lightning.cli import LightningCLI, ArgsType
 
 
 def cli_main(args: ArgsType = None):
-    # breakpoint()
     cli = LightningCLI(args=args)
-    # breakpoint()
     cli.trainer.fit(model=cli.model, datamodule=cli.datamodule)
 
 
